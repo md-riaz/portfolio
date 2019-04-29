cc00aaa30a9f2114fb549990c7d4dddc4a41da97
MD Riaz <riazmd582@gmail.com>
2019-04-30 00:41:16 +0600

    form updated

--- js/main.js
+++ js/main.js
@@ -1,31 +1,33 @@
+function _(id){ return document.getElementById(id); } //Shortcode function
+
 // Select DOM Items
-const menuBtn = document.querySelector('.menu-btn');
-const menu = document.querySelector('.menu');
-const menuNav = document.querySelector('.menu-nav');
-const menuBranding = document.querySelector('.menu-branding');
-const navItems = document.querySelectorAll('.nav-item');
+const menuBtn = document.querySelector(".menu-btn");
+const menu = document.querySelector(".menu");
+const menuNav = document.querySelector(".menu-nav");
+const menuBranding = document.querySelector(".menu-branding");
+const navItems = document.querySelectorAll(".nav-item");
 
 // Set Initial State Of Menu
 let showMenu = false;
 
-menuBtn.addEventListener('click', toggleMenu);
+menuBtn.addEventListener("click", toggleMenu);
 
 function toggleMenu() {
   if (!showMenu) {
-    menuBtn.classList.add('close');
-    menu.classList.add('show');
-    menuNav.classList.add('show');
-    menuBranding.classList.add('show');
-    navItems.forEach(item => item.classList.add('show'));
+    menuBtn.classList.add("close");
+    menu.classList.add("show");
+    menuNav.classList.add("show");
+    menuBranding.classList.add("show");
+    navItems.forEach(item => item.classList.add("show"));
 
     // Set Menu State
     showMenu = true;
   } else {
-    menuBtn.classList.remove('close');
-    menu.classList.remove('show');
-    menuNav.classList.remove('show');
-    menuBranding.classList.remove('show');
-    navItems.forEach(item => item.classList.remove('show'));
+    menuBtn.classList.remove("close");
+    menu.classList.remove("show");
+    menuNav.classList.remove("show");
+    menuBranding.classList.remove("show");
+    navItems.forEach(item => item.classList.remove("show"));
 
     // Set Menu State
     showMenu = false;
@@ -34,18 +36,41 @@ function toggleMenu() {
 
 //for loading check storage
 var checked = JSON.parse(sessionStorage.getItem("change-theme"));
-document.getElementById("change-theme").checked = checked;
+_("change-theme").checked = checked;
 //
-if (JSON.parse(sessionStorage.getItem('dark-theme-enabled'))) {
-  document.body.classList.add('light-mode');
+if (JSON.parse(sessionStorage.getItem("dark-theme-enabled"))) {
+  document.body.classList.add("light-mode");
 }
 
 //click on theme change
-document.getElementById('change-theme').addEventListener('click', function () {
+_("change-theme").addEventListener("click", function() {
   //setting checkbox active status
-  var checkbox = document.getElementById("change-theme");
+  var checkbox = _("change-theme");
   sessionStorage.setItem("change-theme", checkbox.checked);
   //theme changing with light mode
-  let darkThemeEnabled = document.body.classList.toggle('light-mode');
-  sessionStorage.setItem('dark-theme-enabled', darkThemeEnabled);
+  let darkThemeEnabled = document.body.classList.toggle("light-mode");
+  sessionStorage.setItem("dark-theme-enabled", darkThemeEnabled);
 });
+
+// ajax form submit
+function submitForm(){
+	_("mybtn").disabled = true;
+	_("status").innerHTML = 'please wait ...';
+	var formdata = new FormData();
+	formdata.append( "n", _("n").value );
+	formdata.append( "e", _("e").value );
+	formdata.append( "m", _("m").value );
+	var ajax = new XMLHttpRequest();
+	ajax.open( "POST", "contact.php" );
+	ajax.onreadystatechange = function() {
+		if(ajax.readyState == 4 && ajax.status == 200) {
+			if(ajax.responseText == "success"){
+				_("my_form").innerHTML = '<h2>Thanks '+_("n").value+', your message has been sent.</h2>';
+			} else {
+				_("status").innerHTML = ajax.responseText;
+				_("mybtn").disabled = false;
+			}
+		}
+	}
+	ajax.send( formdata );
+}
