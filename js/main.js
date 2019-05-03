b1a8643797dc56fbc6fb751cbbb7a1393b911526
MD Riaz <riazmd582@gmail.com>
2019-05-03 09:01:12 +0600

    contact form updated

--- js/main.js
+++ js/main.js
@@ -1,4 +1,6 @@
-function _(id){ return document.getElementById(id); } //Shortcode function
+﻿function _(id) {
+  return document.getElementById(id);
+} //Shortcode function
 
 // Select DOM Items
 const menuBtn = document.querySelector(".menu-btn");
@@ -53,24 +55,25 @@ _("change-theme").addEventListener("click", function() {
 });
 
 // ajax form submit
-function submitForm(){
-	_("mybtn").disabled = true;
-	_("status").innerHTML = 'please wait ...';
-	var formdata = new FormData();
-	formdata.append( "n", _("n").value );
-	formdata.append( "e", _("e").value );
-	formdata.append( "m", _("m").value );
-	var ajax = new XMLHttpRequest();
-	ajax.open( "POST", "../php/contact.php" );
-	ajax.onreadystatechange = function() {
-		if(ajax.readyState == 4 && ajax.status == 200) {
-			if(ajax.responseText == "success"){
-				_("my_form").innerHTML = '<h2>Thanks '+_("n").value+', your message has been sent.</h2>';
-			} else {
-				_("status").innerHTML = ajax.responseText;
-				_("mybtn").disabled = false;
-			}
-		}
-	}
-	ajax.send( formdata );
+function submitForm() {
+  _("mybtn").disabled = true;
+  _("status").innerHTML = "please wait ...";
+  var formdata = new FormData();
+  formdata.append("n", _("n").value);
+  formdata.append("e", _("e").value);
+  formdata.append("m", _("m").value);
+  var ajax = new XMLHttpRequest();
+  ajax.open("POST", "../php/contact.php");
+  ajax.onreadystatechange = function() {
+    if (ajax.readyState == 4 && ajax.status == 200) {
+      if (ajax.responseText == "success") {
+        _("my_form").innerHTML =
+          "<h2>Thanks " + _("n").value + ", your message has been sent.</h2>";
+      } else {
+        _("status").innerHTML = ajax.responseText;
+        _("mybtn").disabled = false;
+      }
+    }
+  };
+  ajax.send(formdata);
 }
--- php/contact.php
+++ php/contact.php
@@ -1,27 +1,17 @@
 ﻿<?php
-// Check for empty fields
-if(empty($_POST['name'])  		||
-   empty($_POST['email']) 		||
-   empty($_POST['message'])	||
-   !filter_var($_POST['email'],FILTER_VALIDATE_EMAIL))
-   {
-	echo "No information Provided!";
-	return false;
-   }
-   
-if( isset($_POST['name']) && isset($_POST['email']) && isset($_POST['message']) ){
-	$n = $_POST['name']; // HINT: use preg_replace() to filter the data
-	$e = $_POST['email'];
-	$m = nl2br($_POST['message']);
+if( isset($_POST['n']) && isset($_POST['e']) && isset($_POST['m']) ){
+	$n = $_POST['n']; // HINT: use preg_replace() to filter the data
+	$e = $_POST['e'];
+	$m = nl2br($_POST['m']);
 	$to = "bpimdriaz@gmail.com";	
 	$from = $e;
 	$subject = 'Contact Form Message';
-	$message = '<b>Name:</b> '.$n.' <br><b>Email:</b> '.$e.' <p></b>'.$m.'</p>';
+	$message = '<b>Name:</b> '.$n.' <br><b>Email:</b> '.$e.' <p>'.$m.'</p>';
 	$headers = "From: $from\n";
 	$headers .= "MIME-Version: 1.0\n";
 	$headers .= "Content-type: text/html; charset=iso-8859-1\n";
 	if( mail($to, $subject, $message, $headers) ){
-		echo "Mail has been send Successfully";
+		echo "success";
 	} else {
 		echo "The server failed to send the message. Please try again later.";
 	}
